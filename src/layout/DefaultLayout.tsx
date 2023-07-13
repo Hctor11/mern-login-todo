63f59acfdc3daf04be35adfb7620c21d236b4f87
hector <hectorrrivera1010@gmail.com>
2023-07-12 21:29:00 -0600

    add Default Layout component

---
+++ src/layout/DefaultLayout.tsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom'
+
+interface DefaultLayoutOptions{
+    children: React.ReactNode
+}
+
+const DefaultLayout = ({children}:DefaultLayoutOptions) => {
+  return (
+    <>
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/Signup'>Singup</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            {children}
+        </main>
+    </>
+  )
+}
+
+export default DefaultLayout
