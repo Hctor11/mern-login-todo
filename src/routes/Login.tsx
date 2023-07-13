fe404b24d58b8ce4a94983158f225d89409fb9ad
hector <hectorrrivera1010@gmail.com>
2023-07-12 23:32:49 -0600

    add user auth on sign up and login

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,19 +1,19 @@
+import DefaultLayout from "../layout/DefaultLayout";
 
 const Login = () => {
-  
-    return (
+  return (
     <>
-      <form>
-        <h1> Log in </h1>
-        <label htmlFor="">Username</label>
-        <input type="text" />
-        <label htmlFor="">Password</label>
-        <input type="password" />
+      <DefaultLayout>
+        <form>
+          <h1> Log in </h1>
+          <label htmlFor="">Username</label>
+          <input type="text" />
+          <label htmlFor="">Password</label>
+          <input type="password" />
 
-        <button>
-          Iniciar Sesion
-        </button>
-      </form>
+          <button>Iniciar Sesion</button>
+        </form>
+      </DefaultLayout>
     </>
   );
 };
